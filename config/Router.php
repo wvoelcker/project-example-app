8691b153c3f9017771b2d0886e3125d84c0451cb
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-03-09 15:23:58 +0000

    Simplifying router

--- config/Router.php
+++ config/Router.php
@@ -2,10 +2,7 @@
 namespace ProjectExampleApp\Config;
 
 class Router extends \WillV\Project\Router {
-	protected $catchExceptions = false;
-
 	protected function setUp() {
 		$this->get("/", "home");
 	}
-
 }
