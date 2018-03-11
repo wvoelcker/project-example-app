1733ac2568150f09551c351a1cf6f97e00ad40ff
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-03-11 17:39:27 +0000

    Update global.php

--- global.php
+++ global.php
@@ -1,6 +1,6 @@
 <?php
 use WillV\Project\App;
 
-$projectRoot = realpath(__DIR__);
+$projectRoot = __DIR__;
 require_once $projectRoot."/vendor/autoload.php";
 $APP = App::bootstrap($projectRoot, "ProjectExampleApp");
