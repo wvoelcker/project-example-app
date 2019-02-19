c7436ffaa035067e5f8d469500197c94c5ac5853
Will Voelcker <wvoelcker@yahoo.co.uk>
2019-02-19 19:03:19 +0000

    Bring up to date with latest (fixed) version of project

--- global.php
+++ global.php
@@ -1,6 +1,8 @@
 <?php
 use WillV\Project\App;
+use ProjectExampleApp\Config\Router;
+use ProjectExampleApp\Config\EnvironmentList;
 
 $projectRoot = __DIR__;
 require_once $projectRoot."/vendor/autoload.php";
-$APP = App::bootstrap($projectRoot, "ProjectExampleApp");
+$APP = App::bootstrap($projectRoot, "ProjectExampleApp", EnvironmentList::create());
