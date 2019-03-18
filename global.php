ca7d29d986ad2d1c5b21b30bdf5c3cdf8c85354d
Will Voelcker <wvoelcker@yahoo.co.uk>
2019-03-18 15:41:37 +0000

    Updating project example app to correlate with latest fixes and remove unused code

--- global.php
+++ global.php
@@ -1,8 +1,6 @@
 <?php
 use WillV\Project\App;
-use ProjectExampleApp\Config\Router;
-use ProjectExampleApp\Config\EnvironmentList;
 
 $projectRoot = __DIR__;
 require_once $projectRoot."/vendor/autoload.php";
-$APP = App::bootstrap($projectRoot, "ProjectExampleApp", EnvironmentList::create());
+$APP = App::bootstrap($projectRoot, "ProjectExampleApp");
